fe304e171487d3b7d84a3e8b30a587e8d14e1d11
Tohru <tohrudj0711@gmail.com>
2026-05-01 11:18:02 +0700

    Initial Commit

---
+++ BuDe/BuDeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BuDeApp.swift
+//  BuDe
+//
+//  Created by Tohru Djunaedi Sato on 01/05/26.
+//
+
+import SwiftUI
+
+@main
+struct BuDeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
