cb296632e40e4cf4352be684e0e70472e1ac8ef0
gab <gabriellawidjaja05@gmail.com>
2026-05-03 23:52:48 +0700

    feat: add CameraPreview

--- BuDe/Features/Scan/ConditionCard.swift
+++ BuDe/Features/Scan/ConditionCard.swift
@@ -8,11 +8,30 @@
 import SwiftUI
 
 struct ConditionCard: View {
+    let condition: Potato
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(alignment: .leading) {
+            Image(systemName: "questionmark.fill")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 50, height: 50)
+            
+            Text(condition.action)
+                .font(.headline)
+                .foregroundStyle(condition.isRecommended ? .green : .red)
+            
+            Text(condition.name)
+                .font(.caption)
+                .foregroundStyle(condition.isRecommended ? .green : .red)
+        }
+        .padding()
+        .background(condition.isRecommended ? .green.opacity(0.1) : .red.opacity(0.15))
+        .background(.ultraThinMaterial)
+        .cornerRadius(16)
     }
 }
 
 #Preview {
-    ConditionCard()
+    ConditionCard(condition: Potato.data[0])
 }
