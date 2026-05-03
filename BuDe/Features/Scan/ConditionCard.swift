090049ecc0d382179d52b73fed8ff2fc4e3ad75f
gab <gabriellawidjaja05@gmail.com>
2026-05-03 23:51:57 +0700

    feat: add condition card component

---
+++ BuDe/Features/Scan/ConditionCard.swift
@@ -0,0 +1,18 @@
+//
+//  ConditionCard.swift
+//  BuDe
+//
+//  Created by Gabriella Angelina Widjaja on 03/05/26.
+//
+
+import SwiftUI
+
+struct ConditionCard: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ConditionCard()
+}
---
+++ BuDe/Features/Scan/Views/CameraPreview.swift
@@ -0,0 +1,18 @@
+//
+//  CameraPreview.swift
+//  BuDe
+//
+//  Created by Gabriella Angelina Widjaja on 03/05/26.
+//
+
+import SwiftUI
+
+struct CameraPreview: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CameraPreview()
+}
