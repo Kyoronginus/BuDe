30b00a406ce040fe6baf97d4f6d087a47aeeee70
Davin P <davinpidada2005@gmail.com>
2026-05-03 16:27:34 +0700

    add the camera manager file for sending camera pixel to ML model

--- BuDe/Core/Camera/CameraManager.swift
+++ BuDe/Core/Camera/CameraManager.swift
@@ -5,3 +5,64 @@
 //  Created by Tohru Djunaedi Sato on 01/05/26.
 //
 
+import Foundation
+import AVFoundation // access camera
+import CoreVideo // for cvpixelbuffer format image
+import Combine
+
+class CameraManager: NSObject, ObservableObject {
+    
+    let camSession = AVCaptureSession()
+    private let videoOutput = AVCaptureVideoDataOutput() // output gambar
+    var onFrameCaptured: ((CVPixelBuffer) -> Void)?
+    override init() {
+        super.init()
+        setupCamera()
+    }
+    
+    private func setupCamera(){
+        camSession.beginConfiguration()
+        
+        guard let videoDevice = AVCaptureDevice.default(.builtInWideAngleCamera, for: .video, position: .back)
+        else {
+            print("cam not found!")
+            return
+        }
+        
+        do{
+            let videoInput = try AVCaptureDeviceInput(device: videoDevice)
+            if camSession.canAddInput(videoInput){
+                camSession.addInput(videoInput)
+            }
+            
+            videoOutput.setSampleBufferDelegate(self, queue: DispatchQueue(label: "videoQueue"))
+            if camSession.canAddOutput(videoOutput){
+                camSession.addOutput(videoOutput)
+            }
+        } catch{
+            print("failed: \(error.localizedDescription)")
+        }
+        
+        camSession.commitConfiguration()
+    }
+    
+    func start(){
+        DispatchQueue.global(qos: .background).async{
+            self.camSession.startRunning()
+        }
+    }
+    
+    func stop(){
+        self.camSession.stopRunning()
+    }
+    
+}
+
+// intinya output dari gambar akan dikirim ke scanviewmodel dengan onframecaptured
+extension CameraManager: AVCaptureVideoDataOutputSampleBufferDelegate{
+    func captureOutput(_ output: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection){
+        guard let pixelBuffer = CMSampleBufferGetImageBuffer(sampleBuffer) else {return}
+        self.onFrameCaptured?(pixelBuffer)
+    }
+}
+
