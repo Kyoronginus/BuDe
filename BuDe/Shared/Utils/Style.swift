d4806ef94a3b16fba555f0f2c7fde066b74d2970
gab <gabriellawidjaja05@gmail.com>
2026-05-04 10:57:26 +0700

    fix: hapus storage tips

---
+++ BuDe/Shared/Utils/Style.swift
@@ -0,0 +1,18 @@
+//
+//  Style.swift
+//  BuDe
+//
+//  Created by Gabriella Angelina Widjaja on 04/05/26.
+//
+
+import SwiftUI
+
+struct Style: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Style()
+}
