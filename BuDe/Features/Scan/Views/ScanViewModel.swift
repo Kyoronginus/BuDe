74bd3ef5c808cb54b1f277da84ff8dd302b4edc9
gab <gabriellawidjaja05@gmail.com>
2026-05-04 08:34:55 +0700

    feat: add CameraPreview

--- BuDe/Features/Scan/Views/ScanViewModel.swift
+++ BuDe/Features/Scan/Views/ScanViewModel.swift
@@ -1,18 +1,18 @@
 //
-//  ScanView.swift
+//  ScanViewModel.swift
 //  BuDe
 //
-//  Created by Gabriella Angelina Widjaja on 03/05/26.
+//  Created by Gabriella Angelina Widjaja on 04/05/26.
 //
 
 import SwiftUI
 
-struct ScanView: View {
+struct ScanViewModel: View {
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
 #Preview {
-    ScanView()
+    ScanViewModel()
 }
